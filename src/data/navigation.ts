bd996a5f6090635df352e72b9f26344c566b05d6
ezazee <yukiirima123@gmail.com>
2025-10-08 14:52:25 +0700

    transaction

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -11,6 +11,6 @@ import type { NavItem } from "@shared/types/types";
 export const bottomNavItemsData: NavItem[] = [
   { name: "Home", href: "/", icon: HomeIcon },
   { name: "Produk", href: "/all-product", icon: AllProduct },
-  { name: "Transaksi", href: "/transaction", icon: TransactionIcon },
+  { name: "Transaksi", href: "/account/transaction", icon: TransactionIcon },
   { name: "Akun", href: "/account", icon: AccountIcon },
 ];
